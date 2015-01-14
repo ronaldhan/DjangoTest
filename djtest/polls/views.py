0e800186bb552d2b8ee88afbc6906c79cacde564
ronaldhan <ronald.han7@gmail.com>
2015-01-14 11:41:16 +0800

    use the generic view to rewritte the url and views

--- djtest/polls/views.py
+++ djtest/polls/views.py
@@ -7,25 +7,25 @@ from django.template import RequestContext
 from django.core.urlresolvers import reverse
 
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    # t = loader.get_template('polls/index.html')
-    # c = Context({
-    #     'latest_poll_list': latest_poll_list
-    # })
-    # output = ', '.join([p.question for p in latest_poll_list])
-    # return HttpResponse(t.render(c))
-    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+# def index(request):
+#     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+#     # t = loader.get_template('polls/index.html')
+#     # c = Context({
+#     #     'latest_poll_list': latest_poll_list
+#     # })
+#     # output = ', '.join([p.question for p in latest_poll_list])
+#     # return HttpResponse(t.render(c))
+#     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 
-def detail(request, poll_id):
-    # return HttpResponse("You're looking at he results of poll %s" % poll_id)
-    # try:
-    #     p = Poll.objects.get(pk=poll_id)
-    # except Poll.DoesNotExist:
-    #     raise Http404
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
+# def detail(request, poll_id):
+#     # return HttpResponse("You're looking at he results of poll %s" % poll_id)
+#     # try:
+#     #     p = Poll.objects.get(pk=poll_id)
+#     # except Poll.DoesNotExist:
+#     #     raise Http404
+#     p = get_object_or_404(Poll, pk=poll_id)
+#     return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
 
 
 def vote(request, poll_id):
@@ -40,9 +40,9 @@ def vote(request, poll_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
 
 
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/results.html', {'poll': p})
+# def results(request, poll_id):
+#     p = get_object_or_404(Poll, pk=poll_id)
+#     return render_to_response('polls/results.html', {'poll': p})
